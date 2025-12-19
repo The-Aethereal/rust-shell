43126d6f3d7701f6a836b57310175c72e06c4b4f
The-Aethereal <suryanshctf05@gmail.com>
2025-12-20 01:52:54 +0530

    echo command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     loop {
-         print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut t: String= String::new();
-    io::stdin().read_line(&mut t).unwrap();
-    println!("{}: command not found", t.trim())
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+
+        let cmd = command.trim();
+
+        if let Some(msg) = cmd.strip_prefix("echo ") {
+            println!("{msg}");
+        } else {
+            match cmd {
+                "" => continue,
+                "exit" => return,
+                _ => println!("{cmd}: command not found"),
+            }
+        }
     }
 }
