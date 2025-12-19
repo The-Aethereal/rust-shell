fb17c5de0e94247daf8f03316b0813ffec2f38aa
The-Aethereal <suryanshctf05@gmail.com>
2025-12-19 07:07:23 +0530

    creating repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+         print!("$ ");
+    io::stdout().flush().unwrap();
+    let mut t: String= String::new();
+    io::stdin().read_line(&mut t).unwrap();
+    println!("{}: command not found", t.trim())
+    }
 }
